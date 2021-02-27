3fb6de8619fe2715ef6b85d7f025ed80cba21dca
Victor Fernandes <victorfernandes.matias@gmail.com>
2021-02-27 12:36:33 -0300

    Fixed the TypeError exception when try to get a city with expired cache.

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 config = {
     "DEBUG": DEBUG,
-    "CACHE_TYPE": "simple"
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 10
 }
 app.config.from_mapping(config)
 cache = Cache(app)
@@ -32,38 +33,67 @@ cache = Cache(app)
 
 class Weather(Resource):
 
+    def get_weather_data(self, city_name):
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_Key')}")
+        return response
+
     def get(self, city_name: str):
+
         data = request.view_args["city_name"]
 
-        cached_data = [cached for cached in cache.cache._cache]
+        error_message = None
+
+        cached_data = deque([cached for cached in cache.cache._cache])
 
         if city_name not in cached_data:
 
-            response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_Key')}")
+            response = self.get_weather_data(city_name)
 
             if response.status_code == 200:
 
-                if len(cached_data) >= 5:
+                if len(cached_data) > 5:
                     cache.delete(cached_data.popleft())
 
                 payload = {
                     f"{city_name}": response.json()
                 }
 
-                cache.set(key=city_name, value=payload, timeout=10)
+                cache.set(key=city_name, value=payload)
             else:
-                return {
-                    "message": "Sorry We coudn't find the specified city."
-                }
+                error_message = "Sorry We coudn't find the specified city."
 
-        content = {
-            "current_city": {
-                "city_name": cache.get(city_name)[city_name]["name"],
-                "degree": f"{int(cache.get(city_name)[city_name]['main']['temp']) - 273.15:.2f}",
-                "state": cache.get(city_name)[city_name]["weather"][0]["description"]
-            },
-            "other_cities": []
-        }
+        if error_message is not None:
+            content = {
+                "error_message": error_message,
+                "other_cities": []
+            }
+        else:
+            try:
+                cached_city = cache.get(city_name)
+                content = {
+                    "current_city": {
+                        "city_name": cached_city[city_name]["name"],
+                        "degree": f"{int(cached_city[city_name]['main']['temp']) - 273.15:.2f}",
+                        "state": cached_city[city_name]["weather"][0]["description"]
+                    },
+                    "other_cities": []
+                }
+            except TypeError as err:
+                logger.info("Cache already expired, it has been updated")
+                response = self.get_weather_data(city_name)
+                payload = {
+                    f"{city_name}": response.json()
+                }
+                cache.set(key=city_name, value=payload)
+                cached_city = cache.get(city_name)
+                content = {
+                    "current_city": {
+                        "city_name": cached_city[city_name]["name"],
+                        "degree": f"{int(cached_city[city_name]['main']['temp']) - 273.15:.2f}",
+                        "state": cached_city[city_name]["weather"][0]["description"]
+                    },
+                    "other_cities": []
+                }
 
         if cached_data is not None:
             for cont, city in enumerate(cached_data):
