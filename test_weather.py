faa3a9428e26a0df2f3ad353b210a1762d8b7d80
Victor Fernandes <victorfernandes.matias@gmail.com>
2021-02-27 18:38:45 -0300

    Added test_file and start to handle tests.

---
+++ test_weather.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+
+import pytest
+from loguru import logger
+from requests_html import HTMLSession
+from wsgiadapter import WSGIAdapter
+
+from app import flask_app
+from app import get_weather_data
+
+
+API_KEY = os.getenv('API_Key')
+
+
+valid_cities_list = ["Recife", "Itambé", "João Pessoa", "Pedras de Fogo"]
+invalid_Cities_list = ["SDFsdfs", "sadsdasda", "SADSASDA"]
+
+
+@pytest.fixture
+def client():
+    flask_app.config["TESTING"] = True
+    client = flask_app.test_client()
+    yield client
+
+#def test_weather_endpoint(client):
+#    for city in valid_cities_list:
+#        response = get_weather_data(city)
+#        assert 200 == response.status_code
+
+def test_post_valid_cities(client):
+    for city in valid_cities_list:
+        response = HTMLSession()
+        response.mount("http://test", WSGIAdapter(flask_app))
+        payload = {
+            "city_name": city
+        }
+        response = response.post("http://test/weather", data=payload)
+        assert 200 == response.status_code
+        assert city in response.html.html
+
+
+
+
+
+
+
