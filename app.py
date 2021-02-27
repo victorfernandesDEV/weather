90f4d63f78a9884639dcc03ff169346d5317a0ec
Victor Fernandes <victorfernandes.matias@gmail.com>
2021-02-27 16:47:30 -0300

    Addes an other view to handle responses lookup using url kwargs.

--- app.py
+++ app.py
@@ -143,5 +143,78 @@ def weather_index():
         return make_response(render_template("index.html", **content))
 
 
+@app.route("/weather/<string:city_name>", methods=["GET"])
+def get_data_by_city_name(city_name: str):
+
+    error_message = None
+
+    cached_data = deque([cached for cached in cache.cache._cache])
+    if city_name not in cached_data:
+
+        response = get_weather_data(city_name)
+
+        if response.status_code == 200:
+
+            if len(cached_data) >= 5:
+                cache.delete(cached_data.popleft())
+
+            payload = {
+                f"{city_name}": response.json()
+            }
+
+            cache.set(key=city_name, value=payload)
+        else:
+            error_message = "Sorry We coudn't find the specified city."
+
+    if error_message is not None:
+        content = {
+            "error_message": error_message,
+            "other_cities": []
+        }
+    else:
+        try:
+            cached_city = cache.get(city_name)
+            content = {
+                "current_city": {
+                    "city_name": cached_city[city_name]["name"],
+                    "degree": f"{int(cached_city[city_name]['main']['temp']) - 273.15:.2f}",
+                    "state": cached_city[city_name]["weather"][0]["description"]
+                },
+                "other_cities": []
+            }
+        except TypeError as err:
+            logger.info("Cache already expired, it has been updated")
+            response = get_weather_data(city_name)
+            payload = {
+                f"{city_name}": response.json()
+            }
+            cache.set(key=city_name, value=payload)
+            cached_city = cache.get(city_name)
+            content = {
+                "current_city": {
+                    "city_name": cached_city[city_name]["name"],
+                    "degree": f"{int(cached_city[city_name]['main']['temp']) - 273.15:.2f}",
+                    "state": cached_city[city_name]["weather"][0]["description"]
+                },
+                "other_cities": []
+            }
+
+    cached_data = deque([cached for cached in cache.cache._cache])
+
+    if cached_data is not None and len(cached_data) > 1:
+        for cont, city in enumerate(reversed(cached_data)):
+            try:
+                city_data = cache.get(city)
+                payload = {
+                    "city_name": city_data[city]["name"],
+                    "degree": f"{int(city_data[city]['main']['temp']) - 273.15:.2f}",
+                    "state": city_data[city]["weather"][0]["description"]
+                }
+                content["other_cities"].append(payload)
+            except TypeError as err:
+                cache.delete(key=city)
+    return make_response(render_template('index.html', **content))
+
+
 if __name__ == "__main__":
     app.run(debug=DEBUG)
